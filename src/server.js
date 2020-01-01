0bfe8992053b0ff88cc0a7e6f5d921afa6e758c3
Jodi N Kansagor <joedvice@gmail.com>
2019-12-31 17:01:26 -0800

    endpoints started, getting tests set up

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const Hapi = require('hapi');
 require('dotenv').config();
 const Mongoose = require('mongoose');
+const Nirvana = require('../lib/models/Nirvana');
 
 require('../lib/utils/connect')();
 
@@ -12,15 +13,74 @@ const server = new Hapi.Server({
 
 //routes
 server.route({
-  method: 'GET',
+  method: 'POST',
   path: '/',
-  handler: (request, h) => {
-    return 'I am the home route';
+  handler: async (request, h) => {
+
+    try {
+      const nirvana = new Nirvana(request.payload);
+      const result = await nirvana.save();
+      return h.response(result);
+    } catch (error) {
+      return h.response(error).code(500);
+    }
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/nirvana',
+  handler: async (request, h) => {
+    try {
+      const nirvana = await Nirvana.find().exec();
+      return h.response(nirvana);
+    } catch (error) {
+      return h.response(error).code(500);
+    }
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: 'nirvana/${id}',
+  handler: async (request, h) => {
+    try {
+      const nirvana = await Nirvana.findById(request.params.id).exec();
+      return h.response(nirvana);
+    } catch (error) {
+      return h.response(error).code(500);
+    }
+  }
+});
+
+server.route({
+  method: 'PATCH',
+  path: 'nirvana/${id}',
+  handler: async (request, h) => {
+    try {
+      const result = new Nirvana.findByIdAndUpdate(request.params.id, request.payload, { new: true });
+      return h.response(result);
+    } catch (error) {
+      return h.response(error).code(500);
+    }
+  }
+});
+
+server.route({
+  method: 'DELETE',
+  path: '/nirvana/${id}',
+  handler: async (request, h) => {
+    try {
+      var result = await Nirvana.findByIdAndDelete(request.params.id);
+      return h.response(result);
+    } catch (error) {
+      return h.response(error).code(500);
+    }
   }
 });
 
 //define start function
-const launch = async() => {
+const launch = async () => {
   try {
     await server.start();
   } catch (err) {
